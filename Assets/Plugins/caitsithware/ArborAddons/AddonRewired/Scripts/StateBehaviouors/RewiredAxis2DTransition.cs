c8bbb2019cd3a6541e1995c5ac8b9175e13e781f
mssuzuki <mssuzuki@caitsithware.com>
2018-07-19 14:03:48 +0900

    added RewiredAxis2DTransition

---
+++ Assets/Plugins/caitsithware/ArborAddons/AddonRewired/Scripts/StateBehaviouors/RewiredAxis2DTransition.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Arbor;
+
+namespace caitsithware.ArborAddons.AddonRewired.StateBehaviours
+{
+	using Arbor;
+	using Rewired;
+
+	[AddBehaviourMenu("caitsithware/Rewired/RewiredAxis2DTransition")]
+	[AddComponentMenu("")]
+	public class RewiredAxis2DTransition : StateBehaviour
+	{
+		[SerializeField]
+		private FlexibleBool m_IsSystemPlayer = new FlexibleBool(false);
+
+		[SerializeField]
+		private FlexibleString m_PlayerName = new FlexibleString("Player0");
+
+		[SerializeField]
+		private FlexibleString m_XAxisActionName = new FlexibleString("Move Horizontal");
+
+		[SerializeField]
+		private FlexibleString m_YAxisActionName = new FlexibleString("Move Vertical");
+
+		[SerializeField]
+		private OutputSlotVector2 m_OutputAxisValue = new OutputSlotVector2();
+
+		[SerializeField]
+		private StateLink m_OnAxis2D = new StateLink();
+
+		private Player m_Player;
+
+		private string m_CachedXAxisActionName;
+		private string m_CachedYAxisActionName;
+
+		// Use this for enter state
+		public override void OnStateBegin()
+		{
+			if (m_IsSystemPlayer.value)
+			{
+				m_Player = ReInput.players.GetSystemPlayer();
+			}
+			else
+			{
+				m_Player = ReInput.players.GetPlayer(m_PlayerName.value);
+			}
+
+			m_CachedXAxisActionName = m_XAxisActionName.value;
+			m_CachedYAxisActionName = m_YAxisActionName.value;
+		}
+
+		void CheckTransition()
+		{
+			if (m_Player == null || string.IsNullOrEmpty(m_CachedXAxisActionName) || string.IsNullOrEmpty(m_CachedYAxisActionName))
+			{
+				return;
+			}
+
+			Vector2 axisValue = m_Player.GetAxis2D(m_CachedXAxisActionName,m_CachedYAxisActionName);
+			if (axisValue.sqrMagnitude != 0.0f)
+			{
+				m_OutputAxisValue.SetValue(axisValue);
+				Transition(m_OnAxis2D);
+			}
+		}
+
+		// OnStateUpdate is called once per frame
+		public override void OnStateUpdate()
+		{
+			CheckTransition();
+		}
+	}
+}
