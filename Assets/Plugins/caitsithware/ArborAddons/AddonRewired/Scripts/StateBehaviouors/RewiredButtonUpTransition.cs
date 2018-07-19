88a84e9a976d5887cc836f1a2ad01070cc1d0b04
caitsithware <5856669+caitsithware@users.noreply.github.com>
2018-07-19 12:35:59 +0900

    added RewiredButtonTransitions

---
+++ Assets/Plugins/caitsithware/ArborAddons/AddonRewired/Scripts/StateBehaviouors/RewiredButtonUpTransition.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace caitsithware.ArborAddons.AddonRewired.StateBehaviours
+{
+	using Arbor;
+	using Rewired;
+
+	[AddBehaviourMenu("caitsithware/Rewired/RewiredButtonUpTransition")]
+	[AddComponentMenu("")]
+	public class RewiredButtonUpTransition : StateBehaviour
+	{
+		[SerializeField]
+		private FlexibleBool m_IsSystemPlayer = new FlexibleBool(false);
+
+		[SerializeField]
+		private FlexibleString m_PlayerName = new FlexibleString("Player0");
+
+		[SerializeField]
+		private FlexibleString m_ActionName = new FlexibleString("");
+
+		[SerializeField]
+		private StateLink m_OnButtonUp= new StateLink();
+
+		private Player m_Player;
+
+		private string m_CachedActionName;
+		
+		// Use this for enter state
+		public override void OnStateBegin()
+		{
+			if (m_IsSystemPlayer.value)
+			{
+				m_Player = ReInput.players.GetSystemPlayer();
+			}
+			else
+			{
+				m_Player = ReInput.players.GetPlayer(m_PlayerName.value);
+			}
+
+			m_CachedActionName = (m_Player != null) ? m_ActionName.value : "";
+		}
+
+		// OnStateUpdate is called once per frame
+		public override void OnStateUpdate()
+		{
+			if (m_Player != null && !string.IsNullOrEmpty(m_CachedActionName) && m_Player.GetButtonUp(m_CachedActionName))
+			{
+				Transition(m_OnButtonUp);
+			}
+		}
+	}
+}
