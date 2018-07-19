0da99085c8f8d7cef2e45eb0b6ab39610f2aff85
mssuzuki <mssuzuki@caitsithware.com>
2018-07-19 13:16:41 +0900

    added AxisContributionType

--- Assets/Plugins/caitsithware/ArborAddons/AddonRewired/Scripts/StateBehaviouors/RewiredButtonDownTransition.cs
+++ Assets/Plugins/caitsithware/ArborAddons/AddonRewired/Scripts/StateBehaviouors/RewiredButtonDownTransition.cs
@@ -21,6 +21,9 @@ namespace caitsithware.ArborAddons.AddonRewired.StateBehaviours
 		private FlexibleString m_ActionName = new FlexibleString("");
 
 		[SerializeField]
+		private AxisContributionType m_AxisContribution = AxisContributionType.Any;
+
+		[SerializeField]
 		private StateLink m_OnButtonDown = new StateLink();
 
 		private Player m_Player;
@@ -42,10 +45,30 @@ namespace caitsithware.ArborAddons.AddonRewired.StateBehaviours
 			m_CachedActionName = (m_Player != null) ? m_ActionName.value : "";
 		}
 
+		bool CheckTransition()
+		{
+			if (m_Player == null || string.IsNullOrEmpty(m_CachedActionName))
+			{
+				return false;
+			}
+
+			switch (m_AxisContribution)
+			{
+				case AxisContributionType.Any:
+					return m_Player.GetButtonDown(m_CachedActionName) || m_Player.GetNegativeButtonDown(m_CachedActionName);
+				case AxisContributionType.Positive:
+					return m_Player.GetButtonDown(m_CachedActionName);
+				case AxisContributionType.Negative:
+					return m_Player.GetNegativeButtonDown(m_CachedActionName);
+			}
+
+			return false;
+		}
+
 		// OnStateUpdate is called once per frame
 		public override void OnStateUpdate()
 		{
-			if (m_Player != null && !string.IsNullOrEmpty(m_CachedActionName) && m_Player.GetButtonDown(m_CachedActionName))
+			if (CheckTransition())
 			{
 				Transition(m_OnButtonDown);
 			}
