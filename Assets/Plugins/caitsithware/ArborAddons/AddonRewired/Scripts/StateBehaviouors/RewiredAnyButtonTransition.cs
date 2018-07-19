dc78a939e9975c313fe17d4c63c5ff1ef1227133
mssuzuki <mssuzuki@caitsithware.com>
2018-07-19 12:01:55 +0900

    added RewiredAnyButtonTransitions

---
+++ Assets/Plugins/caitsithware/ArborAddons/AddonRewired/Scripts/StateBehaviouors/RewiredAnyButtonTransition.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace caitsithware.ArborAddons.AddonRewired.StateBehaviours
+{
+	using Arbor;
+	using Rewired;
+
+	[AddBehaviourMenu("caitsithware/Rewired/RewiredAnyButtonTransition")]
+	[AddComponentMenu("")]
+	public class RewiredAnyButtonTransition : StateBehaviour
+	{
+		[SerializeField]
+		private FlexibleBool m_IsSystemPlayer = new FlexibleBool(false);
+
+		[SerializeField]
+		private FlexibleString m_PlayerName = new FlexibleString("Player0");
+
+		[SerializeField]
+		private StateLink m_OnButtonDown = new StateLink();
+
+		private Player m_Player;
+
+		// Use this for enter state
+		public override void OnStateBegin()
+		{
+			if (m_IsSystemPlayer.value)
+			{
+				m_Player = ReInput.players.GetSystemPlayer();
+			}
+			else
+			{
+				m_Player = ReInput.players.GetPlayer(m_PlayerName.value);
+			}
+		}
+
+		// OnStateUpdate is called once per frame
+		public override void OnStateUpdate()
+		{
+			if (m_Player != null && m_Player.GetAnyButton())
+			{
+				Transition(m_OnButtonDown);
+			}
+		}
+	}
+}
