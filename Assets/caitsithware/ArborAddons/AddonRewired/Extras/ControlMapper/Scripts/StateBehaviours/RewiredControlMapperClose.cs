cf3560e0366618f2402ae8d2e92f7a5eea3e1e3f
caitsithware <5856669+caitsithware@users.noreply.github.com>
2018-07-19 20:06:00 +0900

    ControlMapper開閉Behaviour追加

---
+++ Assets/caitsithware/ArborAddons/AddonRewired/Extras/ControlMapper/Scripts/StateBehaviours/RewiredControlMapperClose.cs
@@ -0,0 +1,34 @@
+﻿	using UnityEngine;
+	using System.Collections;
+	using System.Collections.Generic;
+
+namespace caitsithware.ArborAddons.AddonRewired
+{
+	using Arbor;
+	using Rewired.UI.ControlMapper;
+
+	[AddBehaviourMenu("Rewired/ControlMapper/RewiredControlMapperClose")]
+	[AddComponentMenu("")]
+	public class RewiredControlMapperClose : StateBehaviour
+	{
+		[ClassExtends(typeof(ControlMapper))]
+		[SerializeField]
+		private FlexibleComponent m_ControlMapper = new FlexibleComponent();
+
+		[SerializeField]
+		private FlexibleBool m_SaveOnClose = new FlexibleBool(false);
+
+		// Use this for enter state
+		public override void OnStateBegin()
+		{
+			ControlMapper controlMapper = m_ControlMapper.value as ControlMapper;
+
+			if (controlMapper == null)
+			{
+				return;
+			}
+
+			controlMapper.Close(m_SaveOnClose.value);
+		}
+	}
+}
