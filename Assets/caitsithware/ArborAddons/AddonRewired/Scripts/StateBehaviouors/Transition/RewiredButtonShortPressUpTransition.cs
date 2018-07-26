1abb03c2948abd955aa6e6e30e1f1f1b13047b07
caitsithware <dev@caitsithware.com>
2018-07-26 14:08:41 +0900

    add state behaviours of Player.GetButtonShortPress

---
+++ Assets/caitsithware/ArborAddons/AddonRewired/Scripts/StateBehaviouors/Transition/RewiredButtonShortPressUpTransition.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace caitsithware.ArborAddons.AddonRewired.StateBehaviours
+{
+	using Arbor;
+	using Rewired;
+
+	[AddBehaviourMenu("Rewired/Transition/RewiredButtonShortPressUpTransition")]
+	[AddComponentMenu("")]
+	public class RewiredButtonShortPressUpTransition : StateBehaviour
+	{
+		[SerializeField]
+		private FlexibleString m_PlayerName = new FlexibleString("Player0");
+
+		[SerializeField]
+		private FlexibleString m_ActionName = new FlexibleString("");
+
+		[SerializeField]
+		private AxisContributionType m_AxisContribution = AxisContributionType.Any;
+
+		[SerializeField]
+		private StateLink m_OnButtonShortPressUp = new StateLink();
+
+		private Player m_Player;
+		private string m_CachedActionName;
+
+		// Use this for enter state
+		public override void OnStateBegin()
+		{
+			m_Player = ReInput.players.GetPlayer(m_PlayerName.value);
+			m_CachedActionName = m_ActionName.value;
+		}
+
+		bool CheckTransition()
+		{
+			if (m_Player == null || string.IsNullOrEmpty(m_CachedActionName))
+			{
+				return false;
+			}
+
+			switch (m_AxisContribution)
+			{
+				case AxisContributionType.Any:
+					return m_Player.GetButtonShortPressUp(m_CachedActionName) || m_Player.GetNegativeButtonShortPressUp(m_CachedActionName);
+				case AxisContributionType.Positive:
+					return m_Player.GetButtonShortPressUp(m_CachedActionName);
+				case AxisContributionType.Negative:
+					return m_Player.GetNegativeButtonShortPressUp(m_CachedActionName);
+			}
+
+			return false;
+		}
+
+		// OnStateUpdate is called once per frame
+		public override void OnStateUpdate()
+		{
+			if (CheckTransition())
+			{
+				Transition(m_OnButtonShortPressUp);
+			}
+		}
+	}
+}
